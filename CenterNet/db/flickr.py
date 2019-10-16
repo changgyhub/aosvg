49f3f9b2776118a74030ac4935e289204686b1b7
irsisyphus <cgao96@gmail.com>
2019-10-16 18:20:49 -0400

    misc

--- CenterNet/db/flickr.py
+++ CenterNet/db/flickr.py
@@ -28,12 +28,12 @@ def bbox_iou(box1, box2):
     b2_x1, b2_y1, b2_x2, b2_y2 = box2[0], box2[1], box2[2], box2[3]
 
     # get the coordinates of the intersection rectangle
-    inter_rect_x1 = np.max(b1_x1, b2_x1)
-    inter_rect_y1 = np.max(b1_y1, b2_y1)
-    inter_rect_x2 = np.min(b1_x2, b2_x2)
-    inter_rect_y2 = np.min(b1_y2, b2_y2)
+    inter_rect_x1 = max(b1_x1, b2_x1)
+    inter_rect_y1 = max(b1_y1, b2_y1)
+    inter_rect_x2 = min(b1_x2, b2_x2)
+    inter_rect_y2 = min(b1_y2, b2_y2)
     # Intersection area
-    inter_area = np.max(inter_rect_x2 - inter_rect_x1, 0) * np.max(inter_rect_y2 - inter_rect_y1, 0)
+    inter_area = max(inter_rect_x2 - inter_rect_x1, 0) * max(inter_rect_y2 - inter_rect_y1, 0)
     # Union Area
     b1_area = (b1_x2 - b1_x1) * (b1_y2 - b1_y1)
     b2_area = (b2_x2 - b2_x1) * (b2_y2 - b2_y1)
